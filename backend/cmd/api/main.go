aff587cb9d6da6bdf4fcda45240ce0f4f4609243
AkKeRise <akkerise@gmail.com>
2025-08-27 22:34:21 +0700

    Initialize Go module and stub server

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+
+	"github.com/truyentan/backend/internal/app"
+)
+
 func main() {
-	// TODO: implement backend API
+	srv := app.NewServer()
+	if err := srv.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
